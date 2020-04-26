270d1f63b5ce9905f63ce4c664405b3a863351bb
Yusuf Aran <yusuf.aran@outlook.de>
2020-04-26 14:27:19 +0200

    fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const features = [
     imageUrl: "img/react-fluent-form-fusion.svg",
     description: (
       <>
-        Stop worriyng about state handling and focus on configuration. An API
+        Stop worrying about state handling and focus on configuration. An API
         designed to fluently read and write configuration for forms.
       </>
     ),
